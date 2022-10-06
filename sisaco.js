bf2511327a5f7c8f5aed857a48f2a9f2aaab1560
SISACO <athulrammp3@gmail.com>
2022-10-06 23:16:50 +0530

    make it better

--- sisaco.js
+++ sisaco.js
@@ -555,20 +555,21 @@ case 'ttp': {
                      break
 
 case 'ig': {       
-  var tcttt = [];
-  await instagramGetUrl(args[0]).then(async response => { 
-  for (let url_list of response) {     
-    tcttt.push(url_list)   
-    sisaco.sendFile(m.chat, tcttt, 'instagram.mp4', `『 - - - - - ɪɴsᴛᴀɢʀᴀᴍ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ - - - - - 』*`, m)
+  
+let res =  await instagramGetUrl(args[0])
+  for (let url_list of Object.keys(res)) {     
+    let results = res[url_list];
+    sisaco.sendFile(m.chat, results, 'instagram.mp4', `『 - - - - - ɪɴsᴛᴀɢʀᴀᴍ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ - - - - - 』*`, m)
  } 
-}   )  
+  
 }
 break
 case 'igs':{
 let res = await axios.get(`https://zenzapis.xyz/downloader/instagram/story?apikey=86f5d90096&username=${text}`)
 let anu = res.data
 for (let result of anu.url_list) {
-     sisaco.sendMedia(m.chat, result, text, 'morou', m,{contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'support group', title: 'packname', body: 'grupo de soporte', thumbnailUrl: tu, sourceUrl: dygp }}}, {asDocument: true})
+     
+     sisaco.sendMedia(m.chat, results, text, 'morou', m,{contextInfo: { externalAdReply: { mediaUrl: dygp, mediaType: 'VIDEO', description: 'support group', title: 'packname', body: 'grupo de soporte', thumbnailUrl: tu, sourceUrl: dygp }}}, {asDocument: true})
     
   } 
 }
