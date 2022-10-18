8ec70cbd66c2d2c0b2fff9b57c971d55b696f6e4
SISACO <athulrammp3@gmail.com>
2022-10-18 20:04:10 +0530

    make it better

--- neoxrApi.js
+++ neoxrApi.js
@@ -1,4 +1,4 @@
-
+const {fetchJson} = require('./lib/myfunc')
 module.exports = class NeoxrApi {
    baseUrl = 'https://api.neoxr.my.id/api'
    apiKey = null
@@ -8,97 +8,97 @@ module.exports = class NeoxrApi {
    }
    
    check = async () => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/check/' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/check/' + this.apiKey)
       return json
    }
    
    podcast = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/podcast?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/podcast?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
    
    fb = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/fb?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/fb?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    ig = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/ig?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/ig?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    igs = async (str) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/igstory?username=' + str + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/igstory?username=' + str + '&apikey=' + this.apiKey)
       return json
    }
    
    line = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/line?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/line?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    pin = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/pin?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/pin?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    mediafire = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/mediafire?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/mediafire?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    tiktok = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/tiktok?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/tiktok?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    twitter = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/twitter?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/twitter?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
 
    soundcloud = async (url) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/soundcloud?url=' + url + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/soundcloud?url=' + url + '&apikey=' + this.apiKey)
       return json
    }
    
    rexdl = async (str) => {
-      let json = str.match('rexdl.com') ? await sisaco.fetchJson(this.baseUrl + '/rexdl-get?url=' + str + '&apikey=' + this.apiKey) : await sisaco.fetchJson(this.baseUrl + '/rexdl?q=' + encodeURIComponent(str) + '&apikey=' + this.apiKey)
+      let json = str.match('rexdl.com') ? await fetchJson(this.baseUrl + '/rexdl-get?url=' + str + '&apikey=' + this.apiKey) : await fetchJson(this.baseUrl + '/rexdl?q=' + encodeURIComponent(str) + '&apikey=' + this.apiKey)
       return json
    }
    
    pinterest = async (query) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/pinterest?q=' + query + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/pinterest?q=' + query + '&apikey=' + this.apiKey)
       return json
    }
    
    soundcloud = async (str) => {
-      let json = str.match('soundcloud.com') ? await sisaco.fetchJson(this.baseUrl + '/soundcloud?url=' + str + '&apikey=' + this.apiKey) : await sisaco.fetchJson(this.baseUrl + '/soundcloud-search?q=' + str + '&apikey=' + this.apiKey)
+      let json = str.match('soundcloud.com') ? await fetchJson(this.baseUrl + '/soundcloud?url=' + str + '&apikey=' + this.apiKey) : await fetchJson(this.baseUrl + '/soundcloud-search?q=' + str + '&apikey=' + this.apiKey)
       return json
    }
    
    apk = async (query, no) => {
       if (query && no) {
-         let json = await sisaco.fetchJson(this.baseUrl + '/apk?q=' + query + '&no=' + no + '&apikey=' + this.apiKey)
+         let json = await fetchJson(this.baseUrl + '/apk?q=' + query + '&no=' + no + '&apikey=' + this.apiKey)
          return json
       } else if (query) {
-         let json = await sisaco.fetchJson(this.baseUrl + '/apk?q=' + query + '&apikey=' + this.apiKey)
+         let json = await fetchJson(this.baseUrl + '/apk?q=' + query + '&apikey=' + this.apiKey)
          return json
       }
    }
    
    emojimix = async (emoticon) => {
-  	let json = await sisaco.fetchJson(this.baseUrl + '/emoji?q=' + encodeURI(emoticon) + '&apikey=' + this.apiKey)
+  	let json = await fetchJson(this.baseUrl + '/emoji?q=' + encodeURI(emoticon) + '&apikey=' + this.apiKey)
       return json
    }
    
    wallpaper = async (query) => {
-      let json = await sisaco.fetchJson(this.baseUrl + '/wallpaper2?q=' + query + '&apikey=' + this.apiKey)
+      let json = await fetchJson(this.baseUrl + '/wallpaper2?q=' + query + '&apikey=' + this.apiKey)
       return json
    }
    
    sticker = async (str) => {
-      let json = str.match('getstickerpack.com') ? await sisaco.fetchJson(this.baseUrl + '/sticker-get?url=' + str + '&apikey=' + this.apiKey) : await sisaco.fetchJson(this.baseUrl + '/sticker?q=' + encodeURIComponent(str) + '&apikey=' + this.apiKey)
+      let json = str.match('getstickerpack.com') ? await fetchJson(this.baseUrl + '/sticker-get?url=' + str + '&apikey=' + this.apiKey) : await fetchJson(this.baseUrl + '/sticker?q=' + encodeURIComponent(str) + '&apikey=' + this.apiKey)
       return json
    }
    
@@ -111,7 +111,7 @@ module.exports = class NeoxrApi {
    }
    
    brainly = async (query, lang) => {
-  	let json = await sisaco.fetchJson(this.baseUrl + '/brainly?q=' + query + '&iso=' + lang + '&apikey=' + this.apiKey)
+  	let json = await fetchJson(this.baseUrl + '/brainly?q=' + query + '&iso=' + lang + '&apikey=' + this.apiKey)
       return json
    }
 }
